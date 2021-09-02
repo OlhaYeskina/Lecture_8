6743cbcf08c2ef3719acd24b4bc58cec216369da
dinosaur321 <dinosaur321@users.com>
2021-09-02 20:40:39 +0300

    Add test for 3 numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,56 +1,64 @@
+import static java.lang.Integer.parseInt;
 
-
-    public class Calculator {
+public class Calculator {
 
     public int firstValue;
     public int secondValue;
 
 
-    public void StringDelimeter(String str){
+    public int AddStringDelimeter(String str){
+        int sum = 0;
         if(str.isEmpty()){
-            firstValue = 0;
-            secondValue= 0;
-        }else
-        if(str.indexOf(",")>0){
-            String[] subStr;
-            String delimeter = ","; // Разделитель
-            subStr = str.split(delimeter); // Разделения строки str с помощью метода split()
-            if(subStr.length == 1){
-                firstValue = Integer.parseInt(subStr[0]);
-                secondValue = 0;}
-            else {
-                firstValue = Integer.parseInt(subStr[0]);
-                secondValue = Integer.parseInt(subStr[1]);
-            }
-
-        }else{
-            firstValue = Integer.parseInt(str);
-            secondValue = 0;
+            return sum;
         }
+//            firstValue = 0;
+//            secondValue= 0;
+//        }else
+//        if(str.indexOf(",")>0){
+            String[] subStr;
+            String delimeter = "[/,|\n/g]"; // Разделитель
+            subStr = str.split(delimeter); // Разделения строки str с помощью метода split()
+            for (int i = 0; i < subStr.length; i++) {
+                sum+= parseInt(subStr[i]);
+            }
+//            if(subStr.length == 1){
+//                firstValue = Integer.parseInt(subStr[0]);
+//                secondValue = 0;}
+//            else {
+//                firstValue = Integer.parseInt(subStr[0]);
+//                secondValue = Integer.parseInt(subStr[1]);
+//            }
+//
+//        }else{
+//            firstValue = Integer.parseInt(str);
+//            secondValue = 0;
+
+            return sum;
+
 
 
 
 
     }
-        public int addition(String values){
-            StringDelimeter(values);
-            return firstValue+secondValue;
-        }
-
-        public int subtraction(String values){
-
-            return 0;
-        }
-
-        public int multiplication(String values){
-
-            return 0;
-        }
-
-
-        public int division(String values){
-            return 0;
-        }
+//        public int addition(String values){
+//          //  StringDelimeter(values);
+//            return firstValue+secondValue;
+//        }
+//
+//        public int subtraction(String values){
+//
+//            return 0;
+//        }
+//
+//        public int multiplication(String values){
+//
+//            return 0;
+//        }
+//
+//
+//        public int division(String values){
+//            return 0;
+//        }
 
 
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,18 +6,30 @@ public class CalculatorTest {
     @Test
     public void addingEmptyStringCheck(){
         Calculator c = new Calculator();
-        assertEquals(0,c.addition(""));
+        assertEquals(0,c.AddStringDelimeter(""));
     }
 
     @Test
     public void addingOneValueCheck(){
         Calculator c = new Calculator();
-        assertEquals(1,c.addition("1"));
+        assertEquals(1,c.AddStringDelimeter("1"));
     }
 
     @Test
     public void addingTwoValueCheck(){
         Calculator c = new Calculator();
-        assertEquals(3,c.addition("1,2"));
+        assertEquals(3,c.AddStringDelimeter("1,2"));
+    }
+
+    @Test
+    public void addingTreeValueCheck(){
+        Calculator c = new Calculator();
+        assertEquals(6,c.AddStringDelimeter("1,2,3"));
+    }
+
+    @Test
+    public void addingTreeValueCheckWithNewLineBeetweenNumbers(){
+        Calculator c = new Calculator();
+        assertEquals(6,c.AddStringDelimeter("1\n2,3"));
     }
         }
